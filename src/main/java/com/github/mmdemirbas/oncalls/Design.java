51ea734e7c477868680569b1fe45e8d86a455c7e
Muhammed Demirbaş <mmdemirbas@gmail.com>
2018-11-18 01:02:37 +0300

    Simplify domain model

--- src/main/java/com/github/mmdemirbas/oncalls/Design.java
+++ src/main/java/com/github/mmdemirbas/oncalls/Design.java
@@ -8,7 +8,6 @@ import java.time.ZonedDateTime;
 import java.util.Collection;
 import java.util.List;
 import java.util.NavigableMap;
-import java.util.Set;
 
 /**
  * @author Muhammed Demirbaş
@@ -43,25 +42,19 @@ public final class Design {
 
     @Value
     public static final class Patches<Time extends Comparable<? super Time>, V> {
-        Forwardings<Time, V> forwardings;
-        Events<Time, V>      overrides;
+        Events<Time, Forwarding<V>> forwardings;
+        Events<Time, V>             overrides;
     }
 
     @Value
-    public static final class Forwardings<Time extends Comparable<? super Time>, V> {
-        Set<Forwarding<Time, V>> forwardings;
+    public static final class Forwarding<V> {
+        V forwarder;
+        V forwardedTo;
     }
 
     @Value
     public static final class Events<Time extends Comparable<? super Time>, V> {
         NavigableMap<Time, List<Event<Time, V>>> index;
-    }
-
-    @Value
-    public static final class Forwarding<Time extends Comparable<? super Time>, V> {
-        Range<Time> when;
-        V            forwarder;
-        V            forwardedTo;
     }
 
     @Value
