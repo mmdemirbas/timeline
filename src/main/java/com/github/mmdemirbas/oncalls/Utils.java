809a38aabe1288bd68ed02880cc701ee3bc6b63b
Muhammed Demirbaş <mmdemirbas@gmail.com>
2018-12-03 00:30:08 +0300

    Consider null lists as empty lists

--- src/main/java/com/github/mmdemirbas/oncalls/Utils.java
+++ src/main/java/com/github/mmdemirbas/oncalls/Utils.java
@@ -2,7 +2,6 @@ package com.github.mmdemirbas.oncalls;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 import static java.util.Collections.emptyList;
 import static java.util.Collections.unmodifiableList;
@@ -13,7 +12,6 @@ final class Utils {
     }
 
     static <V> List<V> unmodifiableCopyOf(List<? extends V> input) {
-        // todo: bu ve bunun gibi yerlerde list olarak kullanılacak şey null ise empty list kullanılabilir NPE atmak yerine..
-        return unmodifiableList(new ArrayList<>(Objects.requireNonNull(input, "input")));
+        return ((input == null) || input.isEmpty()) ? emptyList() : unmodifiableList(new ArrayList<>(input));
     }
 }
